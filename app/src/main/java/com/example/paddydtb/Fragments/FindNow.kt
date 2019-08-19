5b0440c930d9191347cdad5f1e152bfb5eebcaf3
Chethana Arunodh <chethana0777@gmail.com>
2019-08-19 19:15:17 +0530

    Select image and pass to identification2

--- app/src/main/java/com/example/paddydtb/Fragments/FindNow.kt
+++ app/src/main/java/com/example/paddydtb/Fragments/FindNow.kt
@@ -1,34 +1,45 @@
 package com.example.paddydtb.Fragments
 
+import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.paddydtb.BuildConfig
 
-import com.example.paddydtb.R
-import kotlinx.android.synthetic.main.fragment_findnow.*
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_findnow.view.*
-import kotlinx.android.synthetic.main.fragment_findnow.view.imageView2
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 
+
+
 class FindNow : Fragment() {
+
     private val TAG = "Camera"
     private val CAMERA_REQUEST = 1
     internal lateinit var mCurrentPhotoPath: String
     private val RESULT_LOAD_IMAGE = 1
     private var filePath: Uri? = null
+    private var photoFile: File? = null
+    var completedProcessImagePath = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +69,20 @@ class FindNow : Fragment() {
         return view
     }
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+
+
+                Picasso.get().load("file:" + photoFile!!.getAbsolutePath()).fit().centerInside().into(view!!.imageViewtestsss);
+            Log.e("Capture Image Bug:", "OKKK")
+            }
+
+    }
+
+/*
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -100,28 +125,49 @@ class FindNow : Fragment() {
         }
 
     }
-
-
-    private fun dispatchTakePictureIntent() {
-
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (cameraIntent.resolveActivity(activity!!.packageManager) != null) {
-            // Create the File where the photo should go
-            var photoFile: File? = null
-            try {
-                photoFile = createImageFile()
-            } catch (ex: IOException) {
-                // Error occurred while creating the File
-                Log.i(TAG, "IOException")
-            }
-
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
-                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+*/
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 0) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent()
             }
         }
 
+    }
+
+    private fun dispatchTakePictureIntent() {
+        if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+            //Toast.makeText(activity, "Please allow camera access to take the photo", Toast.LENGTH_LONG).show()
+//            Util.alertDialog("Alert!","Please allow camera access to take the photo",context)
+        } else {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (cameraIntent.resolveActivity(activity!!.packageManager) != null) {
+                try {
+                    photoFile = createImageFile()
+                    completedProcessImagePath = photoFile!!.getAbsolutePath()
+                    // Continue only if the File was successfully createdx
+                    val authority = BuildConfig.APPLICATION_ID + ".provider"
+                    if (photoFile != null) {
+                        var photoURI = FileProvider.getUriForFile(
+                            context!!,
+                            authority,
+                            photoFile!!
+                        )
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST)
+                    }
+                } catch (ex: Exception) {
+                    // Error occurred while creating the File
+                    displayMessage(context!!, "Capture Image Bug: " + ex.message.toString())
+                    Log.e("Capture Image Bug:", ex.message.toString())
+                }
+            }
+        }
+    }
+    private fun displayMessage(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 
     @Throws(IOException::class)
--- app/src/main/java/com/example/paddydtb/Fragments/Identification.kt
+++ app/src/main/java/com/example/paddydtb/Fragments/Identification.kt
@@ -1,19 +1,13 @@
 package com.example.paddydtb.Fragments
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_identification.view.*
-import android.graphics.BitmapFactory
-import android.graphics.Bitmap
 import android.provider.MediaStore
-import kotlinx.android.synthetic.main.fragment_identification.*
-import kotlinx.android.synthetic.main.fragment_identification.view.imageView2
-import java.io.File
+import kotlinx.android.synthetic.main.fragment_identification.view.imageViewtestsss
 import java.io.IOException
 
 
@@ -40,7 +34,7 @@ class Identification : Fragment() {
 
             val bitmap = MediaStore.Images.Media.getBitmap(activity!!.contentResolver, uri)
             //image_main.setImageBitmap(bitmap)
-            view.imageView2.setImageBitmap(bitmap)
+            view.imageViewtestsss.setImageBitmap(bitmap)
 
 
         } catch (e: IOException) {
